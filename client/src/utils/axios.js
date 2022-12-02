1488e8c3f0c41b49868ce2cbf672bc815fcdc4e9
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-12-02 23:19:48 +0300

    upd server url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://happiness-services-backend.onrender.com",
+    withCredentials: true,
 });
 
 export default instance;
